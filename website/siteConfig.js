e5a89665929d5787c465f9aa3b67d87fd6ba1ac4
chrislatorres <me@chrislatorres.com>
2018-09-06 14:37:27 -0500

    change siteConfig url back to docs.webmr.io

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -39,7 +39,7 @@ const users = [
 const siteConfig = {
   title: 'Exokit' /* title for your website */,
   tagline: `Post-screen web engine for AR/VR, written in Javascript.`,
-  url: 'https://webmr.io' /* your website url */,
+  url: 'https://docs.webmr.io' /* your website url */,
   baseUrl: '/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
