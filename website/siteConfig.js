49cb82fec220fa663f63ca174a0d44e1748edf77
chrislatorres <me@chrislatorres.com>
2018-10-13 20:22:05 -0500

    update url to docs.webmr.io

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -39,8 +39,8 @@ const users = [
 const siteConfig = {
   title: 'Exokit' /* title for your website */,
   tagline: `Post-screen web engine for AR/VR, written in Javascript.`,
-  url: 'https://webmr.io' /* your website url */,
-  baseUrl: '/learn/' /* base url for your project */,
+  url: 'https://docs.webmr.io' /* your website url */,
+  baseUrl: '/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
@@ -48,7 +48,7 @@ const siteConfig = {
   // Used for publishing and more
   projectName: 'webmr-docs',
   organizationName: 'webmixedreality',
-  cname: 'webmr.io',
+  cname: 'docs.webmr.io',
   stylesheets: [
     'https://pro.fontawesome.com/releases/v5.1.0/css/all.css'
   ],
