e39d8ea21d8deb0e8dae589963451d92ee438e12
clt <me@me.com>
2018-10-14 01:06:10 +0000

    update siteConfig path

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -40,7 +40,7 @@ const siteConfig = {
   title: 'Exokit' /* title for your website */,
   tagline: `Post-screen web engine for AR/VR, written in Javascript.`,
   url: 'https://webmr.io' /* your website url */,
-  baseUrl: '/doc/' /* base url for your project */,
+  baseUrl: '/learn/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
